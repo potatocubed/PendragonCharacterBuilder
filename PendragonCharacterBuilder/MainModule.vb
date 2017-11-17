242a50323519944f6ba58d1202b88f9c6626708d
Chris Longhurst <Chris.Longhurst@tandf.co.uk>
2017-11-17 10:07:49 +0000

    message

--- PendragonCharacterBuilder/MainModule.vb
+++ PendragonCharacterBuilder/MainModule.vb
@@ -6,7 +6,7 @@
         'It won't assign your attributes or your various bonuses, just the standard stuff.
         'Then it'll output an XML file, then convert that into basic forum bbcode and plain text.
 
-
+        'At this point I'm just dicking around trying to sort branches and wotnot.
     End Sub
 
 End Module
