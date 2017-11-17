c05a41ef3a7263c488ce5e39738da2fe02bd810f
Chris Longhurst <Chris.Longhurst@tandf.co.uk>
2017-11-17 09:55:28 +0000

    Add project files.

---
+++ PendragonCharacterBuilder/MainModule.vb
@@ -0,0 +1,12 @@
+﻿Module MainModule
+
+    Sub Main()
+        'The basic program will ask for a name and gender, names for father and grandfather,
+        'and assume that you're a Salisbury knight from the core book.
+        'It won't assign your attributes or your various bonuses, just the standard stuff.
+        'Then it'll output an XML file, then convert that into basic forum bbcode and plain text.
+
+
+    End Sub
+
+End Module
